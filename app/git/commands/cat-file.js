cf1addc92b6cddeaf87e90f35da510af8889c59a
Anvin.C.Shaju <anvincsg@gmail.com>
2025-04-09 15:42:54 +0530

    implement CatFileCommand execution logic for reading and decompressing Git objects

--- app/git/commands/cat-file.js
+++ app/git/commands/cat-file.js
@@ -1,3 +1,7 @@
+import path from "path";
+import fs from "fs";
+import zlib from "zlib";
+
 class CatFileCommand {
   constructor(flag, commitSHA) {
     this.flag = flag;
@@ -5,7 +9,39 @@ class CatFileCommand {
   }
 
   execute() {
-    console.log({ flag: this.flag, commitSHA: this.commitSHA });
+    // Navigate to .git/objects/commitSHA[0..2]
+    // Read the file .git/objects/commitSHA[0..2]/commitSHA[2..]
+    // Decompress the file
+
+    switch (this.flag) {
+      case "-p":
+        {
+          const folder = this.commitSHA.slice(0, 2);
+          const file = this.commitSHA.slice(2);
+
+          const completePath = path.join(
+            process.cwd(),
+            ".git",
+            "objects",
+            folder,
+            file
+          );
+
+          if (!fs.existsSync(completePath)) {
+            throw new Error(`Not a valid object name ${completePath}`);
+          }
+
+          const fileContents = fs.readFileSync(completePath);
+
+          const outputBuffer = zlib.inflateSync(fileContents);
+          const output = outputBuffer.toString();
+
+          console.log(output);
+        }
+        break;
+      default:
+        throw new Error(`Unknown flag ${this.flag}`);
+    }
   }
 }
 
